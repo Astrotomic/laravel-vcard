9937c74f940fde9990b4caf83523f8727d0ccc36
Avenjamin <avenjamin@users.noreply.github.com>
2023-05-23 22:27:34 +1000

    Add Source field

--- src/Vcard.php
+++ src/Vcard.php
@@ -12,6 +12,7 @@ use Astrotomic\Vcard\Properties\Member;
 use Astrotomic\Vcard\Properties\Org;
 use Astrotomic\Vcard\Properties\Photo;
 use Astrotomic\Vcard\Properties\Role;
+use Astrotomic\Vcard\Properties\Source;
 use Astrotomic\Vcard\Properties\Tel;
 use Astrotomic\Vcard\Properties\Title;
 use Astrotomic\Vcard\Properties\Url;
@@ -161,6 +162,13 @@ class Vcard implements Responsable, Stringable
             $countryName,
             $types
         );
+
+        return $this;
+    }
+
+    public function source(string $source): self
+    {
+        $this->properties[] = new Source($source);
 
         return $this;
     }
